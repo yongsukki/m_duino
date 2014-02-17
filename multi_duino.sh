3a0970964d585265a91cc69c940fa7a4abb806ff
Seungrok Han <muster22@naver.com>
2014-02-17 15:39:49 +0900

    Update multi_duino.sh

--- multi_duino.sh
+++ multi_duino.sh
@@ -1,9 +1,8 @@
-
 sudo apt-get update; sudo apt-get upgrade -y
 wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb
 sudo dpkg -i avrdude_5.10-4_armhf.deb
 sudo chmod 4755 /usr/bin/avrdude
 sudo apt-get install -y arduino
 wget http://project-downloads.drogon.net/gertboard/setup.sh
-sh ./setup.sh
+sudo sh ./setup.sh
 sudo avrdude -p m328p -c gpio

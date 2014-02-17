10d06185fe416becc7569259ee69491146840fad
Seungrok Han <muster22@naver.com>
2014-02-17 21:48:35 +0900

    Update multi_duino.sh

--- multi_duino.sh
+++ multi_duino.sh
@@ -1,3 +1,6 @@
+sudo sed -i "s/console=ttyAMA0,115200 kgdboc=ttyAMA0,115200//" /boot/cmdline.txt
+sudo sed -i "/-L ttyAMA0 115200 vt100/s/T0/#T0/" /etc/inittab
+sudo echo 'KERNEL=="ttyAMA0", SYMLINK+="ttyS0",GROUP="dialout",MODE:=0666' >> /etc/udev/rules.d/85-paperduinopi.rules
 sudo apt-get update; sudo apt-get upgrade -y
 wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb
 sudo dpkg -i avrdude_5.10-4_armhf.deb
